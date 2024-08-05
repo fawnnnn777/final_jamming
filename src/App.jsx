3689f330eae98b1fc65d905b98a2e3be209352c8
Diego Rojas <ale.rgdap@gmail.com>
2024-08-04 19:34:06 -0600

    featching songs again

--- src/App.jsx
+++ src/App.jsx
@@ -52,7 +52,7 @@ function App(){
         let e;
         const r = /([^&;=]+)=?([^&;]*)/g;
         const q = window.location.hash.substring(1);
-        while (e = r.exec(q)) {
+        while (e === r.exec(q)) {
           hashParams[e[1]] = decodeURIComponent(e[2]);
         }
         return hashParams;
